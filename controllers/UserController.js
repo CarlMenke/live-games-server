77a02a45b76a39895b32ab79e88516ce8b36e6a5
carlmenke <carlmenke13@gmail.com>
2022-10-04 14:02:33 -0700

    created sign up function

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const {User , Message, Conversation}  = require('../models')
 const {Op} = require('sequelize')
+const middleware = require('../middleware')
 
 const GetAllUsers = async (req,res) =>{
     try{
@@ -8,14 +9,33 @@ const GetAllUsers = async (req,res) =>{
         throw error
     }
 }
-const CreateUser = async (req,res) => {
+const signup = async (req,res) => {
+    try{
+        const exists = User.findOne({
+            where:{
+                name:req.body.name
+            }
+        })
+        console.log(exists)
+        if(!exists){
+            const {name, password} = req.body
+            let hashedPassword = await middleware.hashPassword(password)
+            const user = await User.create({name:name, password:hashedPassword})
+            res.send({user:user,message:"Welcome!"})
+        }else{
+            res.send({message:"Name Already In Use"})
+        }
+    }catch(error){
+        throw error
+    }
+}
+const login = async (req,res) => {
     try{
 
     }catch(error){
         throw error
     }
 }
-
 const UpdateUser = async (req,res) => {
     try{
 
@@ -41,9 +61,10 @@ const GetUserDetails = async (req,res) => {
 }
 
 module.exports = {
-    CreateUser,
+    signup,
     DeleteUser,
     GetUserDetails,
     UpdateUser,
-    GetAllUsers
+    GetAllUsers,
+    login
 }
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
-Router.post('/create',controller.CreateUser)
+Router.post('/signup',controller.signup)
 Router.get('/details/:userId', controller.GetUserDetails)
 Router.put('/update/:userId',controller.UpdateUser)
 Router.delete('/delete/:userId', controller.DeleteUser)
 Router.get('/all', controller.GetAllUsers)
+Router.get('/login/:userId',controller.login)
 
 module.exports = Router
 
