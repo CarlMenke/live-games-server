3de8742275a79e294e7fa1541c63ec24de1d9eb4
carlmenke <carlmenke13@gmail.com>
2022-10-20 10:18:20 -0700

    added cors header for response

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const bodyParser = require('body-parser')
 const AppRouter = require('./routes/AppRouter')
 const PORT = process.env.PORT || 3001
 const cors = require('cors')
-
-app.use(cors())
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
