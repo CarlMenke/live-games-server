74c12bf088774787f36ddc58238cd3b5e58eb279
gmenke54 <menke.gw@gmail.com>
2022-10-20 11:55:22 -0700

    finalized cors headers for socket.io

--- server.js
+++ server.js
@@ -1,55 +1,66 @@
 //psql server establishment
-const app = require('express')()
-const bodyParser = require('body-parser')
-const AppRouter = require('./routes/AppRouter')
-const PORT = process.env.PORT || 3001
-const cors = require('cors')
+const app = require('express')();
+const bodyParser = require('body-parser');
+const AppRouter = require('./routes/AppRouter');
+const PORT = process.env.PORT || 3001;
+const cors = require('cors');
 
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
-app.use('/api', AppRouter)
-const { User }  = require('./models')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => res.json({ message: 'Server Works' }));
+app.use('/api', AppRouter);
+const { User } = require('./models');
 const server = require('http').createServer(app);
-const io = require('socket.io')(server,{  
-    cors: {
-        withCredentials: false,
-        'Access-Control-Allow-Credentials':"true",
-        origin: "https://sendquick.herokuapp.com",
-        methods: ["GET", "POST"]
-  }});
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'https://sendquick.herokuapp.com',
+    allowedHeaders: ['Access-Control-Allow-Origin'],
+    credentials: true
+  }
+});
 
-io.on("connection", async (socket) => {
-    socket.on('send message', async (recievingUserSocket, sendingUserName) => {
-        const reciever = await User.findOne({where:{socket:recievingUserSocket}})
-        const sender = await User.findOne({where:{name:sendingUserName}})
-        if(reciever.open_chat_with === sendingUserName){
-            socket.to(recievingUserSocket).emit('recieved message', {senderId:sender.id, recieverId:reciever.id})
+io.on('connection', async (socket) => {
+  socket.on('send message', async (recievingUserSocket, sendingUserName) => {
+    const reciever = await User.findOne({
+      where: { socket: recievingUserSocket }
+    });
+    const sender = await User.findOne({ where: { name: sendingUserName } });
+    if (reciever.open_chat_with === sendingUserName) {
+      socket.to(recievingUserSocket).emit('recieved message', {
+        senderId: sender.id,
+        recieverId: reciever.id
+      });
+    }
+  });
+  socket.on('send friend request', (data) => {
+    socket.to(data).emit('recieve friend request', data);
+  });
+  socket.on(
+    'send typing start',
+    async (recievingUserSocket, sendingUserName) => {
+      const reciever = await User.findOne({
+        where: { socket: recievingUserSocket }
+      });
+      if (reciever) {
+        if (reciever.open_chat_with === sendingUserName) {
+          socket.to(recievingUserSocket).emit('recieve typing start');
         }
-    })
-    socket.on('send friend request' , ((data)=>{
-        socket.to(data).emit("recieve friend request", data)
-    }))
-    socket.on('send typing start', async (recievingUserSocket, sendingUserName)=>{
-        const reciever = await User.findOne({where:{socket:recievingUserSocket}})
-        if(reciever){
-            if(reciever.open_chat_with === sendingUserName){
-                socket.to(recievingUserSocket).emit('recieve typing start')
-            }
-        }
-    })
-    socket.on('send typing end', async (recievingUserSocket, sendingUserName)=>{
-        const reciever = await User.findOne({where:{socket:recievingUserSocket}})
-        if(reciever){
-            if(reciever.open_chat_with === sendingUserName){
-                socket.to(recievingUserSocket).emit('recieve typing end')
-            }
-        }
-    })
-})
+      }
+    }
+  );
+  socket.on('send typing end', async (recievingUserSocket, sendingUserName) => {
+    const reciever = await User.findOne({
+      where: { socket: recievingUserSocket }
+    });
+    if (reciever) {
+      if (reciever.open_chat_with === sendingUserName) {
+        socket.to(recievingUserSocket).emit('recieve typing end');
+      }
+    }
+  });
+});
 
-server.listen(PORT, () => console.log(`Socket server listening on port: ${PORT}`))
-
-
-
+server.listen(PORT, () =>
+  console.log(`Socket server listening on port: ${PORT}`)
+);
