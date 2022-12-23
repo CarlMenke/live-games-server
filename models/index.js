d1ccb7000954898f6cc6206157e60612fc8f4dc2
carlmenke <carlmenke13@gmail.com>
2022-12-22 17:47:58 -0800

    added sequelize definiton with DATABSSE_URI

--- models/index.js
+++ models/index.js
@@ -10,13 +10,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 console.log("CONFIG VARIABLE RIGTH AFTER IMPORT!!!!!!",config)
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-console.log("SEQUELIZE VARIABLE RIGTH AFTER INITZALIATION!!!!!!", sequelize)
+let sequelize = new Sequelize(process.env.DATABASE_URI, config);
+
 fs
   .readdirSync(__dirname)
   .filter(file => {
