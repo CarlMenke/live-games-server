fb5d7c98cad0b37c8737815f2dd7e2ed64602b0e
carlmenke <carlmenke13@gmail.com>
2022-12-22 17:57:28 -0800

    added security level

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 console.log("CONFIG VARIABLE RIGTH AFTER IMPORT!!!!!!",config)
 const db = {};
 
-let sequelize = new Sequelize(process.env.DATABASE_URI, config);
+let sequelize = new Sequelize("postgresql://sendquickdb.cgyomgrwl384.us-west-2.rds.amazonaws.com:5432/postgres", config);
 
 fs
   .readdirSync(__dirname)
